e82107969e9733296b9846fa37f5fc809f02d90a
audreyantony <webdev2020.audrey@gmail.com>
2020-05-21 11:20:04 +0200

    changement front end

--- files/content/footer.php
+++ files/content/footer.php
@@ -1,13 +1,3 @@
 <footer>
-    <div class="footer">
-        <div class="socialmedia">
-            <a href="https://www.facebook.com/audrey.antony.calzi" target="_blank"><img src="img/reseauxsociaux/facebook.png" alt="facebook" title="Aller vers ma page Facebook" ></a>
-            <a href="https://www.instagram.com/antony_audrey/?hl=fr" target="_blank"><img src="img/reseauxsociaux/instagram.png" alt="instagram" title="Aller vers ma page Instagram"></a>
-            <a href="https://github.com/audreyantony" target="_blank"><img src="img/reseauxsociaux/github.png" alt="github" title="Aller vers ma page Github"></a>
-            <a href="https://www.linkedin.com/in/audrey-antony-b7754084/" target="_blank"><img src="img/reseauxsociaux/linkedin.png" alt="linkedIn" title="Aller vers ma page LinkedIn"></a>
-        </div>
-        <div class="credit">
-            <p> Audrey Antony © 2020</p>
-        </div>
-    </div>
+            <p> Audrey <br> Antony<br> © 2020</p>
 </footer>
--- files/content/header.php
+++ files/content/header.php
@@ -1,4 +1,5 @@
 <header>
+    <div class="barnav"></div>
     <nav role="navigation">
         <div id="menuToggle">
 
@@ -17,4 +18,10 @@
             </ul>
         </div>
     </nav>
+    <div class="socialmedia">
+        <a href="https://www.facebook.com/audrey.antony.calzi" target="_blank"><img src="img/reseauxsociaux/facebook.png" alt="facebook" title="Aller vers ma page Facebook" ></a>
+        <a href="https://www.instagram.com/antony_audrey/?hl=fr" target="_blank"><img src="img/reseauxsociaux/instagram.png" alt="instagram" title="Aller vers ma page Instagram"></a>
+        <a href="https://github.com/audreyantony" target="_blank"><img src="img/reseauxsociaux/github.png" alt="github" title="Aller vers ma page Github"></a>
+        <a href="https://www.linkedin.com/in/audrey-antony-b7754084/" target="_blank"><img src="img/reseauxsociaux/linkedin.png" alt="linkedIn" title="Aller vers ma page LinkedIn"></a>
+    </div>
 </header>
